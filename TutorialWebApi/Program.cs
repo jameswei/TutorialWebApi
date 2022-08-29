2c5afe170bfef5d62e9b3b917db3e204eba2b771
Jia Wei <wjia@microsoft.com>
2022-08-29 15:45:28 +0800

    add descriptive comments

--- TutorialWebApi/Program.cs
+++ TutorialWebApi/Program.cs
@@ -9,7 +9,9 @@ using Microsoft.Extensions.Logging;
 
 namespace TutorialWebApi
 {
-public class Program
+    // In Program class, ASP.NET configure and launch a generic host, which is responsible for startup and lifetime
+    // management. At a minimum, the host configures a server and a request-response processing pipeline .
+    public class Program
 {
     public static void Main(string[] args)
     {
@@ -17,7 +19,19 @@ public class Program
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
+        // CreateDefaultBuilder performs the following by default:
+        // 1. set content root path to the path returned by GetCurrentDirectory()
+        // 2. load host configurations from Env variables with prefix DOTNET_. and command-line arguments
+        // 3. load app configuration from appsettings.json and appsettings.{Env}.json, secrets manager, Env variables, and command-line arguments
+        // 4. add the following logging providers, Console, Debug, and EventSource
+        // 5. enable scoped validation and dependency validation only when Env is Development.
         Host.CreateDefaultBuilder(args)
+            // ConfigureWebHostDefaults performs the following by default:
+            // 1. load host configuration frrom Env variables with prefix ASPNETCORE_.
+            // 2. set Kestrel server as the web server and configure it using app configuration
+            // 3. add host filtering middleware
+            // 4. add forward headers middleware
+            // 5. enable IIS integration
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
--- TutorialWebApi/Startup.cs
+++ TutorialWebApi/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Hosting;
 
 namespace TutorialWebApi
 {
+    // The Startup class configures services collection and request-response processing pipeline.
+    // It's named as Startup by convension, we can give it any name, as long as specify to the UseStartup<StartupClassName> in the Program class.s 
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -15,11 +17,17 @@ namespace TutorialWebApi
 
         public IConfiguration Configuration { get; }
 
+        // The generated code of Startup class.
+        // We can configure the services required by the application.
+        // Service is a reusable component that provides functionality.
+        // Service can be consumed across the application via Dependency Injection (DI) if registered.
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
+
+        // The generated code of Startup class.
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
